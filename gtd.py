1270935fad752a33535fc8e9a22a171733ff633f
Kevin Dominguez <kevindm181998@gmail.com>
2019-02-27 15:38:24 -0500

    lista las maquinas virtuales beta

--- gtd.py
+++ gtd.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python
 from flask import Flask, jsonify, abort, make_response, request
 import subprocess
+import json
 
 app = Flask(__name__)
 
-lista = {}
+lista = []
 
+# 'VBoxManage list vms'
+# curl -i http://localhost:5000/listar
+#
 @app.route('/listar', methods=['GET'])
 def get_vmListas():
  bashCommand = ['VBoxManage', 'list', 'vms']
  salida = subprocess.run( bashCommand, check=True, stdout=subprocess.PIPE).stdout
- print(salida)
+ salida = salida.decode('utf-8')
+ 
+ # Elimina los caracteres indicados del string
+ salida = salida.replace('\\', '').replace('\'', '').replace('\"', '')
 
- return jsonify({'listas': True})
+ lista = salida.split() # El metodo .split() crea una lista en la que cada palabra es un elemento del String 
+ dicionario = dict(lista[i:i+2] for i in range(0, len(lista), 2)) # Convierte una lista en un dict
+
+ #print("\n[lista]:", lista, "\n") 
+ #print("\n[dict]:", dicionario, "\n")     
+
+ #return jsonify( maquinas = lista )
+ #return jsonify( maquinas = dicionario )
+
+ if len(dicionario) <= 1:
+      dicionario = "No hay maquinas virtuales creadas."
+ return jsonify({'vms': dicionario})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
