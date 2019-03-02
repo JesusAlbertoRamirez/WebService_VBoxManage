e30782a00092482085079659076b7564ce83d04b
Kevin Dominguez <kevindm181998@gmail.com>
2019-03-01 19:25:17 -0500

    running

--- gtd.py
+++ gtd.py
@@ -8,29 +8,29 @@ app = Flask(__name__)
 lista = []
 
 # 'VBoxManage list vms'
-# curl -i http://localhost:5000/listar
+# curl -i http://localhost:5000/listarvm
 #
-@app.route('/listar', methods=['GET'])
+@app.route('/listvm', methods=['GET'])
 def get_vmListas():
  bashCommand = ['VBoxManage', 'list', 'vms']
  salida = subprocess.run( bashCommand, check=True, stdout=subprocess.PIPE).stdout
  salida = salida.decode('utf-8')
- 
  # Elimina los caracteres indicados del string
  salida = salida.replace('\\', '').replace('\'', '').replace('\"', '')
-
  lista = salida.split() # El metodo .split() crea una lista en la que cada palabra es un elemento del String 
  dicionario = dict(lista[i:i+2] for i in range(0, len(lista), 2)) # Convierte una lista en un dict
-
- #print("\n[lista]:", lista, "\n") 
- #print("\n[dict]:", dicionario, "\n")     
-
- #return jsonify( maquinas = lista )
- #return jsonify( maquinas = dicionario )
-
  if len(dicionario) <= 1:
       dicionario = "No hay maquinas virtuales creadas."
  return jsonify({'vms': dicionario})
+
+# 'VBoxManage list vms'
+# curl -i http://localhost:5000/runningvm
+#
+@app.route('/runningvm', methods=['GET'])
+def get_runing():
+	output = subprocess.check_output(['VBoxManage', 'list' , 'runningvms'])
+	str = output.decode('utf-8').splitlines()
+	return jsonify({'list': str})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
